eb04a4ade3a10ad02945ac7749becbb01849dcf1
Camelia <camelia.elkhalidy@gmail.com>
2017-07-14 17:49:47 +0200

    solution to challenge 3_array.rb

--- session2/challenge/3_array.rb
+++ session2/challenge/3_array.rb
@@ -2,10 +2,20 @@
 # returns an array containing every other character
 #
 # example:
-# "abcdefg".every_other_char  # => "aceg"
+# "abcdefg".every_other_char  # => ["a", "c", "e", "g"]
+# a.every_other_char
+# every_other_char(a)
 # "".every_other_char         # => ""
 
 class String
   def every_other_char
+    string_copy = chars()
+    every_other = ""
+      string_copy.each_with_index do |letter , index|
+        if index.even?
+          every_other << letter
+        end
+      end
+      return every_other
   end
 end
